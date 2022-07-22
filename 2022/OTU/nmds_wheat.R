74b33a713aa654e734ccc04e734a99988350639e
KentPhamUky <106180887+KentPhamUky@users.noreply.github.com>
2022-07-22 11:18:20 -0400

    Updates to figures
    Updated 2020 and 2021 figures for upcoming committee meeting

--- 2022/OTU/nmds_wheat.R
+++ 2022/OTU/nmds_wheat.R
@@ -6,8 +6,8 @@ library(plyr)
 library(RColorBrewer)
         
 #sets working directory to target folder
-setwd("C:/Users/kentp/Documents/GitHub/Rhizosphere-sequencing/Wheat 2021") 
-setwd("D:/GitHub/Rhizosphere-sequencing/Wheat 2021")
+setwd("C:/Users/kentp/Documents/GitHub/Rhizosphere-sequencing/2022/OTU") 
+setwd("D:/GitHub/Rhizosphere-sequencing/2022/OTU")
 #reads ub the metadata
 metadata <- read_excel(path="wheat metadata.xlsx")
 
@@ -15,7 +15,7 @@ metadata <- read_excel(path="wheat metadata.xlsx")
 #metadata <- rename_all(.tbl=metadata, .funs=tolower)
 
 #read in the nmds axes
-nmds <- read_tsv(file="Mix.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.nmds.axes",
+nmds <- read_tsv(file="toro.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.opti_mcc.0.03.subsample.thetayc.0.03.lt.ave.nmds.axes",
                  col_types = cols(group=col_character()))
 
 #if your metadata has "sample" as column title, this will be by=c('sample'='group') 
@@ -40,30 +40,6 @@ hull_make <- metadata_nmds %>%
 
 #ggplot, no filter
 
-plot = ggplot(metadata_nmds, aes(x = axis1, y = axis2))   +
-  geom_point(size = 2, aes(  colour = as.factor(combined), shape = Site) ) +
-  theme(axis.text.y = element_text(colour = "black", size = 12, face = "bold"), 
-        axis.text.x = element_text(colour = "black", face = "bold", size = 12), 
-        legend.text = element_text(size = 20, face ="bold", colour ="black"), 
-        legend.position = "right", axis.title.y = element_text(face = "bold", size = 14), 
-        axis.title.x = element_text(face = "bold", size = 14, colour = "black"), 
-        legend.title = element_text(size = 14, colour = "black", face = "bold"), 
-        panel.background = element_blank(), panel.border = element_rect(colour = "black", fill = NA, size = 1.2),
-        legend.key=element_blank()) +  
-  labs(x = "NMDS1", colour = "Treatment", y = "NMDS2", shape = "Site")+ 
- geom_polygon(data = hull_make, alpha = 0.2, aes(fill = factor(combined)), show.legend = FALSE)  +
-  #scale_color_brewer(palette = "Set3") + scale_fill_brewer(palette = "Set3") +
-  ggtitle("2020 Wheat Rhizosphere Microbial Community") + theme(plot.title = element_text(size=20, hjust = 0.5)) +
-  annotate(geom="text", x= -.30, y=-.35, label="Stress = 0.145",
-           color="red", size = 6) 
-  
-  #scale_colour_manual(values = c("#009E73", "#E69F00"))  
-
-plot
-
-
-ggsave("NMDS.tiff", height = 8, width = 10, units = "in")
-
 
 
 
@@ -76,7 +52,7 @@ hull_make2 <- metadata_nmds %>%
 
 hull_make2$Treatment = as.factor(hull_make2$Treatment)
 
-plot2 = ggplot(metadata_nmds, aes(x = axis1, y = axis2, color = as.factor(Treatment), shape = Site))   +
+plot2 = ggplot(metadata_nmds, aes(x = axis1, y = axis2, shape = Site, color = as.factor(Treatment)))   +
   geom_point(size = 2 ) +
   theme(axis.text.y = element_text(colour = "black", size = 12, face = "bold"), 
         axis.text.x = element_text(colour = "black", face = "bold", size = 12), 
@@ -89,9 +65,9 @@ plot2 = ggplot(metadata_nmds, aes(x = axis1, y = axis2, color = as.factor(Treatm
   labs(x = "NMDS1", colour = "Treatment", y = "NMDS2", shape = "Site")+ 
   geom_polygon(data = hull_make2, alpha = 0.2, aes(fill = Treatment, colour = Treatment ), show.legend = FALSE)  +
   #scale_color_brewer(palette = "Set3") + scale_fill_brewer(palette = "Set3") +
-  ggtitle("2020 Wheat Rhizosphere Microbial Community") + theme(plot.title = element_text(size=20, hjust = 0.5)) +
-  annotate(geom="text", x= -.30, y=-.35, label="Stress = 0.145 \n Rsq = 0.911",
-           color="red", size = 6) 
+  ggtitle("2021 Wheat Rhizosphere Microbial Community") + theme(plot.title = element_text(size=20, hjust = 0.5)) +
+  annotate(geom="text", x= -.20, y=-.28, label="Stress = 0.170 \n Rsq = 0.891",
+           color="red", size = 6)
 
 plot2
 ggsave("NMDS better.tiff", height = 8, width = 10, units = "in")
