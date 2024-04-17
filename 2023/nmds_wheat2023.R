478b09a3dcee229f1a291b382ba76c0f66be00db
KentPhamUky <kent.pham@uky.edu>
2024-04-17 12:23:22 -0400

    Update nmds_wheat2023.R

--- 2023/nmds_wheat2023.R
+++ 2023/nmds_wheat2023.R
@@ -17,9 +17,16 @@ Wheat2023 <- inner_join(metadata, nmds, by=c('group'))
 as.factor(metadata_nmds$Treatment)
 ####end####
 
+####Hull option####
+hull = Wheat2023 %>%
+  group_by(Site,Treatment) %>%
+  slice(chull(axis1, axis2))
+####end####
+
 ####2023####
-NMDS2023plot = ggplot(Wheat2023, aes(x = axis1, y = axis2, shape = Site, color = Cashcrop, linetype = Site))   +
+NMDS2023plot = ggplot(Wheat2023, aes(x = axis1, y = axis2, shape = Site, color = Cashcrop, linetype = Site, fill=Cashcrop))   +
   geom_point(size = 2 ) +
+  geom_polygon(data=hull, alpha=0.25)+
   theme(axis.text.y = element_text(colour = "black", size = 12, face = "bold"), 
         axis.text.x = element_text(colour = "black", face = "bold", size = 12), 
         legend.text = element_text(size = 20, face ="bold", colour ="black"), 
@@ -28,8 +35,8 @@ NMDS2023plot = ggplot(Wheat2023, aes(x = axis1, y = axis2, shape = Site, color =
         legend.title = element_text(size = 14, colour = "black", face = "bold"), 
         panel.background = element_blank(), panel.border = element_rect(colour = "black", fill = NA, size = 1.2),
         legend.key=element_blank()) +  
-  labs(x = "NMDS1", colour = "Treatment", y = "NMDS2", shape = "Site")+ 
-  stat_ellipse(lwd=1)+
+  labs(x = "NMDS1", y = "NMDS2")+ 
+  #stat_ellipse(lwd=1)+
   ggtitle("Spring 2023 Wheat Rhizosphere Microbial Community") + theme(plot.title = element_text(size=20, hjust = 0.5)) +
   annotate(geom="text", x= .43, y=.5, label="Stress = 0.151 \n Rsq = 0.911 \n 95% confidence",
            color="black", size = 6) +
