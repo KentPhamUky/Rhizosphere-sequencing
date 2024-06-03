17225d7dd144d2b634b4cb4cb55c7c6285c19b06
KentPhamUky <106180887+KentPhamUky@users.noreply.github.com>
2024-06-03 11:35:13 -0400

    Testdata

--- 2024/Phyloseq test.R
+++ 2024/Phyloseq test.R
@@ -5,6 +5,7 @@ BiocManager::install("phyloseq")
 
 library(ggplot2)
 library(vegan)
+library(plyr)
 library(dplyr)
 library(scales)
 library(grid)
@@ -16,7 +17,7 @@ theme_set(theme_bw())
 
 sharedfile = "X.shared"
 taxfile = "X.taxonomy"
-mapfile = "mothurmetadata.csv"
+mapfile = "mothurmetadatatest.csv"
 
 mothur_data <- import_mothur(mothur_shared_file = sharedfile,
                              mothur_constaxonomy_file = taxfile)
@@ -26,36 +27,56 @@ map <- read.csv(mapfile)
 map <- sample_data(map)
 
 # Assign rownames to be Sample ID's
-rownames(map) <- map$SampleID
+rownames(map) <- map$group
 
 moth_merge <- merge_phyloseq(mothur_data, map)
 colnames(tax_table(moth_merge)) <- c("Kingdom", "Phylum", "Class", 
                                      "Order", "Family", "Genus")
 
-moth_sub <- moth_merge %>%
-  subset_samples(Type == "sample") %>%
-  prune_taxa(taxa_sums(.) > 0, .)
+#moth_sub <- moth_merge %>%
+  #subset_samples(Type == "Cashcrop") %>%
+ # prune_taxa(taxa_sums(.) > 0, .)
 
 
 
-Clean <- moth_sub %>%
+Clean <- moth_merge %>%
   subset_taxa(
     Kingdom == "Bacteria" &
       Family  != "mitochondria" &
       Class   != "Chloroplast"
   )
 ####Metadata####
-
+#Not needed due to us subsampling-
 # Make a data frame with a column for the read counts of each sample
 sample_sum_df <- data.frame(sum = sample_sums(Clean))
 ggplot(sample_sum_df, aes(x = sum)) + 
-  geom_histogram(color = "black", fill = "indianred", binwidth = 2500) +
+  geom_histogram(color = "black", fill = "indianred", binwidth = 100) +
   ggtitle("Distribution of sample sequencing depth") + 
   xlab("Read counts") +
   theme(axis.title.y = element_blank())
 smin <- min(sample_sums(Clean))
 smean <- mean(sample_sums(Clean))
 smax <- max(sample_sums(Clean))
+
+####RankedAbundance####
+topN <- 100
+most_abundant_taxa = sort(taxa_sums(Clean), TRUE)[1:topN]
+
+#extract most_abundant_taxa from mouse_data
+Top_100_OTUs <- prune_taxa(names(most_abundant_taxa), Clean)
+
+# create a dataframe with the counts per otu
+otu_sums <- data.frame(taxa_sums(Top_100_OTUs))
+
+# use the dataframe to plot the top 100 OTUs in a Rank abundance curve.
+ggplot(otu_sums,aes(x=reorder(row.names(otu_sums), -taxa_sums.Top_100_OTUs.), y=taxa_sums.Top_100_OTUs.)) + 
+  geom_bar(stat="identity",colour="black",fill="darkturquoise")  +
+  xlab("OTU Rank") + ylab("Number of Sequences per OTU") +
+  scale_x_discrete(expand = c(0,0)) + 
+  scale_y_continuous(expand = c(0,0)) + theme_classic() +
+  ggtitle("Rank Abundance Curve of the Top 100 OTUs") + 
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
 
 ####Phylum Graph####
 
@@ -72,21 +93,20 @@ phylum_colors <- c(
   "#AD6F3B", "#673770","#D14285", "#652926", "#C84248", 
   "#8569D5", "#5E738F","#D1A33D", "#8A7C64", "#599861"
 )
-ggplot(Clean_phylum, aes(x = Plot, y = Abundance, fill = Phylum)) + 
-  facet_grid(Station~.) +
+ggplot(Clean_phylum, aes(x = reorder(Sample,Cashcrop), y = Abundance, fill = Phylum)) + 
+  #facet_grid(Station~.) +
   geom_bar(stat = "identity") +
   scale_fill_manual(values = phylum_colors) +
-  #scale_x_discrete(
-   # breaks = c("7/8", "8/4", "9/2", "10/6"),
-   #labels = c("Jul", "Aug", "Sep", "Oct"), 
-   # drop = FALSE
-  #) +
-  # Remove x axis title
   theme(axis.title.x = element_blank()) + 
   #
   guides(fill = guide_legend(reverse = TRUE, keywidth = 1, keyheight = 1)) +
   ylab("Relative Abundance (Phyla > 2%) \n") +
   ggtitle("Phylum Composition of Spring 2024 \n Bacterial Communities by Plot") 
+
+
+
+
+
 
 ####Genus Graph####
 
@@ -129,24 +149,24 @@ cap_ord <- ordinate(
   physeq = Clean, 
   method = "CAP",
   distance = bray,
-  formula = ~ soilpH + P.kg.ha + K.kg.ha + Ca.kg.ha + Mg.kg.ha + Zn.kg.ha
+  formula = ~ soilpH + P.kg.ha. + K.kg.ha. + Ca.kg.ha. + Mg.kg.ha. + Zn.kg.ha.
 )
 
 # CAP plot
 cap_plot <- plot_ordination(
   physeq = Clean, 
   ordination = cap_ord, 
-  color = "CashCrop", 
+  color = "Cashcrop", 
   axes = c(1,2)
 ) + 
   aes(shape = Site) + 
-  geom_point(aes(colour = CashCrop), alpha = 0.4, size = 4) + 
+  geom_point(aes(colour = Cashcrop), alpha = 0.4, size = 4) + 
   geom_point(colour = "grey90", size = 1.5) + 
   scale_color_manual(values = c("#a65628", "red", "#ffae19", "#4daf4a", 
                                 "#1919ff", "darkorchid3", "magenta")
   )
 
-
+cap_plot
 # Now add the environmental variables as arrows
 arrowmat <- vegan::scores(cap_ord, display = "bp")
 
