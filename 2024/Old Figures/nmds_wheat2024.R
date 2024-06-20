9e6140439fde477741847a03f2fd79fe6d2eefea
KentPhamUky <kent.pham@uky.edu>
2024-06-20 12:55:06 -0400

    Update nmds_wheat2024.R

--- 2024/Old Figures/nmds_wheat2024.R
+++ 2024/Old Figures/nmds_wheat2024.R
@@ -10,15 +10,15 @@ library(grid)
 #reads in the metadata
 metadata <- read_excel(path="Metadata2024.xlsx")
 
-#read in the nmds axes
-nmds <- read_tsv(file="demi.thetayc.axes",col_types = cols(group=col_character()))
+#read in the PCoA axes
+PCoA <- read_tsv(file="Y4.braycurtis.axes",col_types = cols(group=col_character()))
 #Combine data
-Wheat2024 <- inner_join(metadata, nmds, by=c('group'))
-as.factor(metadata_nmds$Treatment)
+Wheat2024 <- inner_join(metadata, PCoA, by=c('group'))
+
 ####end####
 
 ####2024####
-NMDS2024plot = ggplot(Wheat2024, aes(x = axis1, y = axis2, shape = Site, color = Cashcrop, linetype = Site))   +
+PCoA2024plot = ggplot(Wheat2024, aes(x = axis1, y = axis2, shape = Site, color = Cashcrop, linetype = Site))   +
   geom_point(size = 2 ) +
   theme(axis.text.y = element_text(colour = "black", size = 12, face = "bold"), 
         axis.text.x = element_text(colour = "black", face = "bold", size = 12), 
@@ -28,13 +28,13 @@ NMDS2024plot = ggplot(Wheat2024, aes(x = axis1, y = axis2, shape = Site, color =
         legend.title = element_text(size = 14, colour = "black", face = "bold"), 
         panel.background = element_blank(), panel.border = element_rect(colour = "black", fill = NA, size = 1.2),
         legend.key=element_blank()) +  
-  labs(x = "NMDS1", colour = "Treatment", y = "NMDS2", shape = "Site")+ 
+  labs(x = "PC1", colour = "Treatment", y = "PC2", shape = "Site")+ 
   stat_ellipse(lwd=1)+
   ggtitle("Spring 2024 Wheat Rhizosphere Microbial Community") + theme(plot.title = element_text(size=20, hjust = 0.5)) +
   annotate(geom="text", x= .43, y=.5, label="Stress = 0.151 \n Rsq = 0.911 \n 95% confidence",
            color="black", size = 6) +
   theme_classic()
-NMDS2023plot
-ggsave("NMDS 2024.tiff", height = 8, width = 10, units = "in")
+PCoA2023plot
+ggsave("PCoA 2024.tiff", height = 8, width = 10, units = "in")
 
 ####end####
