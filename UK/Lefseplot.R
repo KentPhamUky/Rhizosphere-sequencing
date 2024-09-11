a439aea33dfceae4ff3560d2605d179d1ba9eb17
KentPhamUky <kent.pham@uky.edu>
2024-09-11 12:56:57 -0400

    Update Lefseplot.R

--- UK/Lefseplot.R
+++ UK/Lefseplot.R
@@ -1,2 +1,10 @@
 #lefse plot
 library(tidyverse)
+library(readxl)
+
+
+metadata <- read_excel(path="Lefseplotyear.xlsx")
+
+plot = ggplot(data, aes(x=name, y=value, colour = Year)) + 
+  geom_bar(stat = "identity") +
+  coord_flip()
